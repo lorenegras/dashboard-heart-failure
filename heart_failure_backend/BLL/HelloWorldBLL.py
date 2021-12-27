91667b35124d5ac2e731a75d3801bcee77a7ab06
lorenegras <gras.lorene@gmail.com>
2021-12-27 11:31:27 +0200

    feat(bll): rename BLL

--- heart_failure_backend/BLL/HelloWorldBLL.py
+++
@@ -1,8 +0,0 @@
-
-class HelloWorldBLL():
-    def get(firstName):
-        result = 'world'
-        # Traitement code métier
-        if(firstName != ''):
-            result = firstName
-        return {'hello': result}
--- heart_failure_backend/BLL/MessageHistoryBLL.py
+++
@@ -1,7 +0,0 @@
-from utils.ApiWeather import ApiWeather
-
-class MessageHistoryBLL():
-    def get(self):
-        # Todo : quand on aura une database stable
-        return ''
-
