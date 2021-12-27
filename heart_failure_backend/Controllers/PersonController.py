fbd158ba0d46e5c22db42e191109d13b0618d0fd
lorenegras <gras.lorene@gmail.com>
2021-12-27 16:04:29 +0200

    feat(person): add search all persons

--- heart_failure_backend/Controllers/PersonController.py
+++ heart_failure_backend/Controllers/PersonController.py
@@ -1,5 +1,5 @@
 from flask_restful import Resource
-from BLL.PersonBLL import PersonBLL
+from heart_failure_backend.BLL.PersonBLL import PersonBLL
 
 class PersonController(Resource):
     def get(self):
