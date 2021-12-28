b37d201ac1a969977c9a421f53fc63fd53e7438d
lorenegras <gras.lorene@gmail.com>
2021-12-28 09:55:01 +0200

    fix: rename DTO

--- heart_failure_backend/DTO/WelcomeDFDTO.py
+++
@@ -1,7 +0,0 @@
-from DTO.GlobalIntentDFDTO import GlobalIntentDFDTO
-
-
-class WelcomeDFDTO(GlobalIntentDFDTO):
-
-    def __init__(self, intent, message):
-        super().__init__(intent, message)
