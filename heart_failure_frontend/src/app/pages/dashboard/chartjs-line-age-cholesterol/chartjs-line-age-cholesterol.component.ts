9df79b20dd2afc2abc41cc18e709e67f811dab1b
pterrat <pierreterrat.pro@gmail.com>
2021-12-28 10:28:47 +0200

    feat(chartjs): setup of charts

--- heart_failure_frontend/src/app/pages/dashboard/chartjs-line-age-cholesterol/chartjs-line-age-cholesterol.component.ts
+++ heart_failure_frontend/src/app/pages/dashboard/chartjs-line-age-cholesterol/chartjs-line-age-cholesterol.component.ts
@@ -1,15 +1,71 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { NbThemeService, NbColorHelper } from '@nebular/theme';
 
 @Component({
   selector: 'chartjs-line-age-cholesterol',
   templateUrl: './chartjs-line-age-cholesterol.component.html',
   styleUrls: ['./chartjs-line-age-cholesterol.component.scss']
 })
-export class ChartjsLineAgeCholesterolComponent implements OnInit {
+export class ChartjsLineAgeCholesterolComponent implements OnDestroy {
 
-  constructor() { }
+  data: any;
+  options: any;
+  themeSubscription: any;
 
-  ngOnInit(): void {
+  constructor(private theme: NbThemeService) {
+    this.themeSubscription = this.theme.getJsTheme().subscribe(config => {
+
+      const colors: any = config.variables;
+      const chartjs: any = config.variables.chartjs;
+
+      this.data = {
+        labels: ['0', '10', '20', '30', '40', '50', '60','70','80','90'],
+        datasets: [{
+          data: [65, 59, 80, 81, 56, 55, 40,110,125,139],
+          label: 'Series A',
+          backgroundColor: NbColorHelper.hexToRgbA(colors.primary, 0.3),
+          borderColor: colors.primary,
+        }
+        ],
+      };
+
+      this.options = {
+        responsive: true,
+        maintainAspectRatio: false,
+        scales: {
+          xAxes: [
+            {
+              gridLines: {
+                display: true,
+                color: chartjs.axisLineColor,
+              },
+              ticks: {
+                fontColor: chartjs.textColor,
+              },
+            },
+          ],
+          yAxes: [
+            {
+              gridLines: {
+                display: true,
+                color: chartjs.axisLineColor,
+              },
+              ticks: {
+                fontColor: chartjs.textColor,
+              },
+            },
+          ],
+        },
+        legend: {
+          labels: {
+            fontColor: chartjs.textColor,
+          },
+        },
+      };
+    });
   }
 
+  ngOnDestroy(): void {
+    this.themeSubscription.unsubscribe();
+  }
 }
--- heart_failure_frontend/src/app/pages/dashboard/chartjs-pie-age-cardiacdisease/chartjs-pie-age-cardiacdisease.component.ts
+++ heart_failure_frontend/src/app/pages/dashboard/chartjs-pie-age-cardiacdisease/chartjs-pie-age-cardiacdisease.component.ts
@@ -1,15 +1,56 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { NbThemeService } from '@nebular/theme';
 
 @Component({
   selector: 'chartjs-pie-age-cardiacdisease',
   templateUrl: './chartjs-pie-age-cardiacdisease.component.html',
   styleUrls: ['./chartjs-pie-age-cardiacdisease.component.scss']
 })
-export class ChartjsPieAgeCardiacdiseaseComponent implements OnInit {
+export class ChartjsPieAgeCardiacdiseaseComponent implements OnDestroy {
 
-  constructor() { }
+  data: any;
+  options: any;
+  themeSubscription: any;
 
-  ngOnInit(): void {
+  constructor(private theme: NbThemeService) {
+    this.themeSubscription = this.theme.getJsTheme().subscribe(config => {
+
+      const colors: any = config.variables;
+      const chartjs: any = config.variables.chartjs;
+
+      this.data = {
+        labels: ['Download Sales', 'In-Store Sales', 'Mail Sales'],
+        datasets: [{
+          data: [300, 500, 100],
+          backgroundColor: [colors.primaryLight, colors.infoLight, colors.successLight],
+        }],
+      };
+
+      this.options = {
+        maintainAspectRatio: false,
+        responsive: true,
+        scales: {
+          xAxes: [
+            {
+              display: false,
+            },
+          ],
+          yAxes: [
+            {
+              display: false,
+            },
+          ],
+        },
+        legend: {
+          labels: {
+            fontColor: chartjs.textColor,
+          },
+        },
+      };
+    });
   }
 
+  ngOnDestroy(): void {
+    this.themeSubscription.unsubscribe();
+  }
 }
