76f4e787bff0d5b4a17ad6b29d6f3652d1f38a4e
lorenegras <gras.lorene@gmail.com>
2021-12-28 10:46:21 +0200

    feat: create sex blood pressure DTO

--- heart_failure_backend/DTO/SexBloodPressureDTO.py
+++ heart_failure_backend/DTO/SexBloodPressureDTO.py
@@ -1,44 +1,19 @@
-class WeatherDTO():
-    temperature_c: None
-    max_temperature_c: None
-    min_temperature_c: None
-    temperature_f: None
-    max_temperature_f: None
-    min_temperature_f: None
-    condition: None
-    humidity: None
+class SexBloodPressureDTO():
+    sex: None
+    blood_pressure: None
 
-    def __init__(self, temperature_c, max_temperature_c, min_temperature_c, temperature_f, max_temperature_f, min_temperature_f, condition, humidity):
-        self.temperature_c = temperature_c
-        self.max_temperature_c = max_temperature_c
-        self.min_temperature_c = min_temperature_c
-        self.temperature_f = temperature_f
-        self.max_temperature_f = max_temperature_f
-        self.min_temperature_f = min_temperature_f
-        self.condition = condition
-        self.humidity = humidity
+    def __init__(self, sex, blood_pressure):
+        self.sex = sex
+        self.blood_pressure = blood_pressure
 
-    def get_temperature_c(self):
-        return self.temperature_c
+    def get_sex(self):
+        return self.sex
 
-    def get_max_temperature_c(self):
-        return self.max_temperature_c
+    def get_blood_pressure(self):
+        return self.blood_pressure
 
-    def get_min_temperature_c(self):
-        return self.min_temperature_c
-
-
-    def get_temperature_f(self):
-        return self.temperature_f
-
-    def get_max_temperature_f(self):
-        return self.max_temperature_f
-
-    def get_min_temperature_f(self):
-        return self.min_temperature_f
-
-    def get_condition(self):
-        return self.condition
-
-    def get_humidity(self):
-        return self.humidity
+    def set_sex(self, sex):
+        self.sex = sex
+    
+    def set_blood_pressure(self, blood_pressure):
+        self.blood_pressure = blood_pressure
