e5e20e40450b26c5b401d6732830e25b6b9c01cc
pterrat <pierreterrat.pro@gmail.com>
2021-12-29 19:20:10 +0200

    feat(ageheartdisease): setup service for age heart disease

--- heart_failure_frontend/src/app/@core/mock/ageheartdisease.service.ts
+++ heart_failure_frontend/src/app/@core/mock/ageheartdisease.service.ts
@@ -1,19 +1,24 @@
 import { AgeHeartDisease, AgeHeartDiseaseData } from '../data/ageheartdisease';
 import { of as observableOf,  Observable } from 'rxjs';
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
+const urlServer = 'http://localhost:5000';
 @Injectable()
 export class AgeheartdiseaseService extends AgeHeartDiseaseData {
-
+  constructor(private http: HttpClient){
+    super();
+  }
   private ageHeartDiseaseList: AgeHeartDisease[]  = [
-    {ageCat: '0 à 15 ans', countHeartDisease: 32 },
-    {ageCat: '16 à 30 ans', countHeartDisease: 65 },
-    {ageCat: '30 à 45 ans', countHeartDisease: 232 },
-    {ageCat: '45 à 60 ans', countHeartDisease: 432 },
-    {ageCat: '60 ans +', countHeartDisease: 832 },
+    {age: '0 à 15 ans', countHeartDisease: 32, heartdisease: 0 },
+    {age: '16 à 30 ans', countHeartDisease: 65,  heartdisease: 1 },
+    {age: '30 à 45 ans', countHeartDisease: 232, heartdisease: 1 },
+    {age: '45 à 60 ans', countHeartDisease: 432,  heartdisease: 0 },
+    {age: '60 ans +', countHeartDisease: 832,  heartdisease: 0 },
   ];
 
   getAgeHeartDisease(): Observable<AgeHeartDisease[]> {
-    return observableOf(this.ageHeartDiseaseList);
+    return this.http.get<AgeHeartDisease[]>(urlServer + '/ageheartdisease');
+    // return observableOf(this.ageHeartDiseaseList);
   }
 }
